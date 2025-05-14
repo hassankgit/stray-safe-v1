fc52c2eeb22d19e22d69a7d291da9086794470ab
hk6562004 <hk656.2004@gmail.com>
2025-05-14 17:26:40 -0400

    switched test user endpoint to UserDto, automapper is working

--- StraySafe.Logic/Users/UserClient.cs
+++ StraySafe.Logic/Users/UserClient.cs
@@ -1,4 +1,5 @@
-﻿using Integration.Supabase.Interfaces;
+﻿using AutoMapper;
+using Integration.Supabase.Interfaces;
 using Integration.Supabase.Models.Auth;
 using StraySafe.Logic.Users.Models;
 
@@ -6,10 +7,12 @@ namespace StraySafe.Logic.Users;
 public class UserClient
 {
     private readonly ISupabaseService _supabaseService;
+    private readonly IMapper _mapper;
 
-    public UserClient(ISupabaseService supabaseService)
+    public UserClient(ISupabaseService supabaseService, IMapper mapper)
     {
         _supabaseService = supabaseService;
+        _mapper = mapper;
     }
 
     public async Task<List<User>> GetAllUsers()
@@ -17,9 +20,11 @@ public class UserClient
         return await _supabaseService.Admin.GetAllUsersAsync();
     }
 
-    public async Task<User> GetCurrentUser()
+    public async Task<UserDto> GetCurrentUser()
     {
-        return await _supabaseService.User.GetCurrentUserAsync();
+        User user = await _supabaseService.User.GetCurrentUserAsync();
+        UserDto userDto = _mapper.Map<UserDto>(user);
+        return userDto;
     }
 
     public async Task<TokenDto> Login(LoginRequest request)
