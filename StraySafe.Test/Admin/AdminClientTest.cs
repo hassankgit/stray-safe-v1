ba7e66bf48776d2d0abc3a17d255b46b540910a3
Hassan Khan <hk832@drexel.edu>
2025-07-16 14:41:36 -0400

    added adminservice tests

---
+++ StraySafe.Test/Admin/AdminClientTest.cs
@@ -0,0 +1,28 @@
+﻿using Integration.Supabase.Interfaces;
+using Integration.Supabase.Models.Auth.Users;
+using Moq;
+using StraySafe.Logic.Admin;
+using StraySafe.Test.MockedServices;
+
+namespace StraySafe.Test.Admin;
+
+[TestClass]
+public class AdminClientTest
+{
+    private Mock<ISupabaseService>? _supabaseServiceMock;
+    private AdminClient? _adminClient;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _supabaseServiceMock = SupabaseServiceMockFactory.Mock();
+        _adminClient = new AdminClient(_supabaseServiceMock.Object);
+    }
+
+    [TestMethod]
+    public async Task GetAllUsers_CalledNormally_GetsAllUsers()
+    {
+        List<User> users = await _adminClient!.GetAllUsers();
+        Assert.AreEqual(2, users.Count);
+    } 
+}
